6a17c6299ce6afcdfbeaa5f79b0915d9b6ff0c2c
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2023-12-18 00:47:23 +0100

    Added new task 3: Palindrome Number - task solved

--- Task_3.playground/Contents.swift
+++ Task_3.playground/Contents.swift
@@ -1,10 +1,36 @@
-import UIKit
 import Foundation
 
-func isPalindrome(_ x: Int) -> Bool {
-
-    return true
+func getDigits(_ x: Int) -> [Int] {
+    
+    return String(describing: x).compactMap {
+        Int(String($0))
+    }
     
 }
 
-isPalindrome(3)
+func isPalindrome(_ x: Int) -> Bool {
+    
+    var countIsPalindrome = false
+    
+    if x < 0 {
+        return countIsPalindrome
+    }
+    
+    let originalArray = getDigits(x)
+    var reversedArray: [Int] = []
+    
+    for arrayReversedElement in originalArray.reversed() {
+        reversedArray.append(arrayReversedElement)
+    }
+    
+    countIsPalindrome = originalArray == reversedArray
+    
+    return countIsPalindrome
+    
+}
+
+
+// Test cases
+print(isPalindrome(1221))
+print(isPalindrome(-1221))
+print(isPalindrome(1234321))
