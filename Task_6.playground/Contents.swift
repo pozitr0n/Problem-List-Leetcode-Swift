005e6cf9da1ca3c8721630431fcc026b153a7367
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-01-04 22:34:12 +0100

    End of the task 6

--- Task_6.playground/Contents.swift
+++ Task_6.playground/Contents.swift
@@ -1,9 +1,39 @@
 import Foundation
 
+let charactersMap: [Character:Character] = [
+    "}": "{",
+    ")": "(",
+    "]": "["
+]
+
 func isValid(_ s: String) -> Bool {
     
-    return false
+    // checking the input string for validity
+    let allowedChars = Set("()[]{}")
+    
+    if !allowedChars.isSuperset(of: s) {
+        return false
+    }
+    
+    var mainStack = [Character]()
+    for currentCharacter in s {
+        
+        let curr = charactersMap[currentCharacter]
+       
+        if curr == nil {
+            mainStack.append(currentCharacter)
+        } else {
+            if mainStack.popLast() != curr {
+                return false
+            }
+        }
+        
+        print(mainStack.count)
+        
+    }
+    
+    return mainStack.count == 0
     
 }
 
-print(isValid(""))
+print(isValid("([])"))
