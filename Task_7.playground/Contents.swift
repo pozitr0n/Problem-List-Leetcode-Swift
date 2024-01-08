c41b4d3a9135919c3db76b89d4a9d0024c18cfb8
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-01-08 03:44:58 +0100

    The end of developing task 7

--- Task_7.playground/Contents.swift
+++ Task_7.playground/Contents.swift
@@ -3,7 +3,48 @@ import Foundation
 
 func longestCommonPrefix(_ strs: [String]) -> String {
  
-    // algorithm
-    return ""
+    var finalString: String = ""
+    var conditionIsTrue: Bool = true
+    var count: Int = 0
+    var minCount: Int = 0
+    
+    if let min = strs.min(by: {$1.count > $0.count}) {
+        minCount = min.count
+    }
+    
+    while conditionIsTrue && count <= minCount - 1 {
+        
+        for elemString in strs {
+            
+            let index = elemString.index(elemString.startIndex, offsetBy: count)
+            
+            if finalString.count != count + 1 {
+                finalString.append(elemString[index])
+            } else {
+                
+                let lastCharacter = finalString.last!
+                
+                if lastCharacter != elemString[index] {
+                    finalString.remove(at: finalString.index(before: finalString.endIndex))
+                    conditionIsTrue = false
+                    break
+                }
+                
+            }
+            
+        }
+        
+        count += 1
+        
+    }
+    
+    if !conditionIsTrue && finalString.isEmpty {
+        finalString = ""
+    }
+    
+    return finalString
     
 }
+
+let strs = ["flowe","flowersdfs","flowerccvxcvx","flowersesdfsdfo", "flowerrueourtoeir"]
+print(longestCommonPrefix(strs))
