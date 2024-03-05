8bda64eebeb94fe8e260465f362ebf7da55aec8a
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-03-05 13:37:35 +0100

    Added changes to the task 12

--- Task_12.playground/Contents.swift
+++ Task_12.playground/Contents.swift
@@ -5,8 +5,26 @@ class Solution {
     
     func majorityElement(_ nums: [Int]) -> Int {
         
+        // array for counting
+        // value:count
+        var dictionaryForCounts = [Int : Int]()
         
+        // counting the values
+        for currentElement in nums {
+            dictionaryForCounts[currentElement] = (dictionaryForCounts[currentElement] ?? 0) + 1
+        }
+        
+        print(dictionaryForCounts)
+        
+        guard let key = dictionaryForCounts.max(by: { $0.1 < $1.1 })?.key else {
+            return 0
+        }
+        
+        return key
         
     }
     
 }
+
+let testArray = [4, 4, 4, 3, 3, 5, 3, 4, 4, 6, 6, 5, 5, 2, 5, 5, 5, 9, 10]
+print(Solution().majorityElement(testArray))
